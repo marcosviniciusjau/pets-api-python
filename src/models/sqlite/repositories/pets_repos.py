37ff70e665a5ff8196877b5ff8ccf151c9146f7c
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2024-07-30 15:50:23 -0300

    creating pets repo

---
+++ src/models/sqlite/repositories/pets_repos.py
@@ -0,0 +1,23 @@
+from src.models.sqlite.entities.pets import PetsTable
+from sqlalchemy.orm.exc import NoResultFound
+class PetsRepos:
+
+  def __init__(self, db_connection):
+    self.__db_connection = db_connection
+
+  def get_all_pets(self)-> list[PetsTable]:
+    with self.__db_connection as database:
+      try:
+        pets = database.query(PetsTable).all()
+        return pets
+      except NoResultFound:
+        return []
+
+  def delete_pets(self, name:str)->None:
+    with self.db_connection_handler.session() as database:
+      try:
+        database.session.query(PetsTable).filter(PetsTable.name == name).delete()
+        database.session.commit()
+      except Exception as exception:
+        database.session.rollback()
+        raise exception
